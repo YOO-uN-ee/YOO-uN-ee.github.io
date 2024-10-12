9604e3057e1a6ee6e73e41c6bd9eab6f35e8cbc5
YOO-uN-ee <jiyoonp0228@gmail.com>
2024-10-12 14:41:53 -0500

    testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yoo-un-ee.github.io',
-  base: 'yoo-un-ee.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
